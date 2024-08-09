bb098501b8d0d7f3956a9aff632957ff3da5bfa0
JimNastic316 <jimnastic316@gmail.com>
2024-08-09 16:25:39 -0400

    Strings, start stop & step
    exercises with strings

--- Section3/HelloWorld/helloworld.py
+++ Section3/HelloWorld/helloworld.py
@@ -1,1 +1,21 @@
 print('Hello, World!')
+print("""This text
+has been split
+over several 
+lines""")
+print("""Number 1 \tThe Larch \nNumber 2 \tThe Horse Chestnut""")
+print("""The pet shop owner said "No, no \'e's uh,...he's resting".'""")
+
+anotherString = """This string has been \
+split over \
+several \
+lines"""
+
+yetAnotherString = 'This string has been \
+split over \
+several \
+lines'
+
+print (anotherString)
+
+print(yetAnotherString)
---
+++ Section3/HelloWorld/strings2.py
@@ -0,0 +1,31 @@
+#         01234567890123
+#         43210987654321
+parrot = "Norwegian Blue"
+
+
+
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print('*' * 10)
+
+#           43210987654321
+# parrot = "Norwegian Blue"
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+print('*' *10)
+
+# Slicing
+print(parrot[0:6])   # Norweg
+
+print('*' *10)
+print(parrot[10:14])
